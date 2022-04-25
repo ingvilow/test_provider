3e94e932b5febca7273d5d667785fdb8a4d92f20
aedthuill <samosh3223@gmail.com>
2022-04-25 12:57:15 +0300

    fix: в соотвествии с правилами surf_lints
    Переделано в соответствии с правилами. Разделено на несколько отдельных экранов (наверное, так?)

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -8,7 +8,8 @@ import 'package:test_provider/provider/counter_provider.dart';
 * я не совсем поняла, что имелось ввиду: из другого файла же нельзя получить доступ к провайдеру.
 * На первом экране у меня провайдер объявлен поверх material app,
 * потом в my_home_page я использую final counterProvider = Provider.of<CounterProvider>(context);
-* имелось ввиду, смогу ли я на третьем экране получить как-то значения из этого  final counterProvider = Provider.of<CounterProvider>(context); ? */
+* имелось ввиду, смогу ли я на третьем экране получить как-то значения из этого  final counterProvider = Provider.of<CounterProvider>(context); ?
+* если так, то они же в разных файлах и не получится так */
 
 
 class ThirdPage extends StatefulWidget {
